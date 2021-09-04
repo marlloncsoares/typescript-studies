f46f9b3aa6cb61387c8bea1ac638662d159c47ed
marlloncosares <marlonmcs@hotmail.com>
2021-09-03 22:13:33 -0300

    Updating data type studies in typescript

--- tipos_de_dados/tipos_de_dados.ts
+++ tipos_de_dados/tipos_de_dados.ts
@@ -26,7 +26,7 @@ console.log('Number binario: ', binario);
 console.log('Number octal: ', octal);
 
 // ----------------------------------------------------------------------------
-// String
+// Tipo String
 // ----------------------------------------------------------------------------
 
 // Declarando uma variável do tipo string
@@ -34,6 +34,23 @@ let name: string = "Marllon Soares"; // Utilizando aspas duplas
 let age: string = '26'; // Utilizando aspas simples
 let text: string = `My name is ${name} and i am ${age} years old.`; // Utilizando templates
 console.log(text);
+
+// ----------------------------------------------------------------------------
+// Tipo Array
+// ----------------------------------------------------------------------------
+
+// Declarando um array de tipo number
+let numbers: number[] = [1, 2, 3];
+console.log(numbers);
+
+// Declarando (de forma diferente) um array de tipo string
+// Poderia ser com string[]
+const months: Array<string> = [
+                                'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 
+                                'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 
+                                'Novembro', 'Dezembro'
+                            ];
+console.log(months);
 
 
 
